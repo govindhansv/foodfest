72bfd9c8c5d27b95961b4c5e2999f7f2ef60ae70
govindhansv <96695721+govindhansv@users.noreply.github.com>
2022-09-14 13:16:07 +0530

    Edit fav route

--- routes/favourite-routes.js
+++ routes/favourite-routes.js
@@ -3,8 +3,8 @@
     const router = express.Router();
     const favouritesController = require("../controllers/favourites-controller");
 
-    router.get("/", favouritesController.getAllFavourites);
-    router.get('/add/:id', favouritesController.addFavourite);
+    router.get("/", requiredlogin, favouritesController.getAllFavourites);
+    router.get('/add/:id', requiredlogin, favouritesController.addFavourite);
     router.get('/:id', favouritesController.getFavouriteById);
     router.get("/delete/:id", favouritesController.deleteFavourite);
 
